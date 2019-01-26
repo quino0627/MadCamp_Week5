8b0126156ba22ccd01ccee43989477b449c7bbf6
jungsub <james319@naver.com>
2019-01-26 22:38:35 +0900

    .

---
+++ Assets/Scripts/SinkClick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkClick : Interactable
+{
+    public GameObject water;
+    
+
+    public override void Interact()
+    {
+        OnClick();
+    }
+
+    void OnClick()
+    {
+        Debug.Log("click!");
+        Cursor.visible = true;
+        water.SetActive(true);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
